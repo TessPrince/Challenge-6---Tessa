a06e8abff35c4774262314f511972258ee3f0415
TessPrince <94005300+TessPrince@users.noreply.github.com>
2022-11-25 18:35:04 -0600

    Removed show added missing_email and alphabetized as well as fixed styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'customers/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/customers/missing_email'
+  get '/customers/alphabetized'
+
+  resources :customers, only: [:index, :missing_email, :alphabetized]
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "customers#index"
 end
